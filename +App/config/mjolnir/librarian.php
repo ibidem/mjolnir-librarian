8c55b9eabfc12b4dc50b45872df33693561efa25
srcspider <source.spider@gmail.com>
2012-10-25 13:15:57 +0300

    dompdf

---
+++ +App/config/mjolnir/librarian.php
@@ -0,0 +1,24 @@
+<?php return array
+	(
+		'books' => array
+			(
+				'Mjölnir' => array
+					(
+						'foreword' => 'mjolnir/foreword',
+						'start' => 'mjolnir/intro',
+						'cover' => 'mjolnir/cover',
+						'style' => 'mjolnir/style'
+					),
+			),
+
+		'chapters' => array
+			(
+				'mjolnir/intro' => array
+					(
+						'idx' => 9000,
+						'after' => null,
+						'source' => 'mjolnir/intro',
+					),
+			),
+
+	); # config
