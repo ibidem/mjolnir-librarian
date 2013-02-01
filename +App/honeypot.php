b8daece963d35ed8ebbadcfd9bad450e14734201
srcspider <source.spider@gmail.com>
2013-02-01 17:42:58 +0200

    wip

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -5,8 +5,8 @@
 
 // HowTo: order honeypot -n 'mjolnir\librarian'
 
-class Driver_Dompdf extends \mjolnir\librarian\Driver_Dompdf { /** @return \mjolnir\librarian\Driver_Dompdf */ static function instance() { return parent::instance(); } }
-class Driver_Wkhtmltopdf extends \mjolnir\librarian\Driver_Wkhtmltopdf { /** @return \mjolnir\librarian\Driver_Wkhtmltopdf */ static function instance() { return parent::instance(); } }
 class Markdown extends \mjolnir\librarian\Markdown {}
 class PDF extends \mjolnir\librarian\PDF {}
-class Task_Librarian extends \mjolnir\librarian\Task_Librarian { /** @return \mjolnir\librarian\Task_Librarian */ static function instance($encoded_task = null) { return parent::instance($encoded_task); } }
+class PDFDriver_Dompdf extends \mjolnir\librarian\PDFDriver_Dompdf { /** @return \mjolnir\librarian\PDFDriver_Dompdf */ static function instance() { return parent::instance(); } }
+class PDFDriver_Wkhtmltopdf extends \mjolnir\librarian\PDFDriver_Wkhtmltopdf { /** @return \mjolnir\librarian\PDFDriver_Wkhtmltopdf */ static function instance() { return parent::instance(); } }
+class Task_Librarian extends \mjolnir\librarian\Task_Librarian { /** @return \mjolnir\librarian\Task_Librarian */ static function instance() { return parent::instance(); } }
